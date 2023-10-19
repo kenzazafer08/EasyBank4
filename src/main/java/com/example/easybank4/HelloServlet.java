aff6285cdd4d5a7a224d7c520188ad0ea826f2d2
unknown <zaaferkenza08@gmail.com>
2023-10-19 14:07:58 +0100

    configuration hibernate

--- src/main/java/com/example/easybank4/HelloServlet.java
+++ src/main/java/com/example/easybank4/HelloServlet.java
@@ -5,10 +5,10 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-
     public void init() {
         message = "Hello World!";
     }
@@ -16,10 +16,10 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+
         out.println("</body></html>");
     }
 
